e488371f6852655e4c90cd57ac6ca2827cfae984
XiaoxuanLiuPKU <xiaoxuanliu@pku.edu.cn>
2019-04-15 15:15:05 -0400

    space

--- include/symbol_selectors/symbol_selector_factory.hpp
+++ include/symbol_selectors/symbol_selector_factory.hpp
@@ -13,17 +13,17 @@ class SymbolSelectorFactory {
 public:
     static SymbolSelector* createSymbolSelector(const int type) {
     if (type == 1)
-		return new SingleCharSS();
+    	return new SingleCharSS();
     else if (type == 2)
-		return new DoubleCharSS();
+    	return new DoubleCharSS();
 	else if (type == 3)
-	    return new NGramSS(3);
+		return new NGramSS(3);
 	else if (type == 4)
-	    return new NGramSS(4);
+		return new NGramSS(4);
 	else if (type == 5)
 		return new HeuristicSS();
 	else
-	    return new SingleCharSS();
+		return new SingleCharSS();
     }
 };
 
