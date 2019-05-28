088585274b89df2ed18daacd190f746e15914334
LiuXiaoxuanPKU <xiaoxuanliu@pku.edu.cn>
2019-05-28 16:51:57 +0000

    merge

--- bench/bench.cpp
+++ bench/bench.cpp
@@ -22,11 +22,13 @@ double getNow() {
 int64_t loadKeys(const std::string& file_name,
 		 std::vector<std::string> &keys,
 		 std::vector<std::string> &keys_shuffle) {
+    int cnt = 0;
     std::ifstream infile(file_name);
     std::string key;
     int64_t total_len = 0;
     while (infile.good()) {
-	infile >> key;
+	cnt++;
+       	infile >> key;
 	keys.push_back(key);
 	keys_shuffle.push_back(key);
 	total_len += key.length();
@@ -45,6 +47,7 @@ void exec(const int encoder_type, const int64_t dict_size_limit,
     uint8_t* buffer = new uint8_t[kLongestCodeLen];
     uint64_t total_enc_len = 0;
     double time_start = getNow();
+    int64_t mem = encoder->memoryUse();
     for (int i = 0; i < (int)keys_shuffle.size(); i++) {
 	total_enc_len += encoder->encode(keys_shuffle[i], buffer);
     }
@@ -57,22 +60,47 @@ void exec(const int encoder_type, const int64_t dict_size_limit,
     std::cout << "Throughput = " << tput << " Mops/s" << std::endl;
     std::cout << "Latency = " << lat << " ns/char" << std::endl;
     std::cout << "CPR = " << cpr << std::endl;
+    std::cout << "Memory = " << mem << std::endl;
+    delete[] buffer;
+    delete encoder;
+
+}
+
+int64_t getPercentKey(std::vector<std::string>& all_keys,
+                   std::vector<std::string>& insert_keys,
+                   double percent) {
+    int64_t insert_len = 0;
+    int step = int(100/percent);
+    for (int i = 0; i < (int)all_keys.size(); i+= step) {
+        insert_keys.push_back(all_keys[i]);
+	insert_len += all_keys[i].length();
+    }
+    std::cout << "Get " << all_keys.size() << " " << insert_keys.size() << " keys" << std::endl;
+    return insert_len;
 }
 
 int main(int argc, char *argv[]) {
-    std::vector<std::string> emails;
-    std::vector<std::string> emails_shuffle;
-    int64_t total_len_email = loadKeys(file_email, emails, emails_shuffle);
-    exec(1, 1000, emails, emails_shuffle, total_len_email);
+    //std::vector<std::string> emails;
+    //std::vector<std::string> emails_shuffle;
+    //std::vector<std::string> emails_insert;
+    //int64_t total_len_email = loadKeys(file_email, emails, emails_shuffle);
+    //int64_t total_len_insert = getPercentKey(emails_shuffle, emails_insert, 10);
+    //exec(5, 65536, emails, emails_insert, total_len_insert);
 
     std::vector<std::string> wikis;
     std::vector<std::string> wikis_shuffle;
-    int64_t total_len_wiki = loadKeys(file_wiki, wikis, wikis_shuffle);
-    exec(1, 1000, wikis, wikis_shuffle, total_len_wiki);
+    loadKeys(file_wiki, wikis, wikis_shuffle);
+    std::vector<std::string> wikis_insert;
+    //exec(1, 1000, wikis, wikis_shuffle, total_len_wiki);
+    int64_t total_len_insert = getPercentKey(wikis_shuffle, wikis_insert, 10);
+    wikis_shuffle.clear();
+    exec(5, 65536, wikis_insert, wikis_insert, total_len_insert);
 
-    std::vector<std::string> urls;
-    std::vector<std::string> urls_shuffle;
-    int64_t total_len_url = loadKeys(file_url, urls, urls_shuffle);
+
+    //std::vector<std::string> urls;
+    //std::vector<std::string> urls_shuffle;
+    //int64_t total_len_url = loadKeys(file_url, urls, urls_shuffle);
+    /*
     exec(1, 1000, urls, urls_shuffle, total_len_url);
 
     exec(2, 65536, emails, emails_shuffle, total_len_email);
@@ -86,6 +114,10 @@ int main(int argc, char *argv[]) {
     exec(4, 65536, emails, emails_shuffle, total_len_email);
     exec(4, 65536, wikis, wikis_shuffle, total_len_wiki);
     exec(4, 65536, urls, urls_shuffle, total_len_url);
+    */
+
+    //exec(4, 65536, emails, emails_shuffle, total_len_email);
+    //exec(5, 65536, urls, urls_shuffle, total_len_url);
     
     return 0;
 }
